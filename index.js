9a6bc3639479daf479ab82294eda1b3c25872f6e
Mahmut Hüdayi Sakarya <sakaryamh@gmail.com>
2022-03-09 14:50:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,88 @@
+
+// Detecting Button Press
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+// Aynı kodu 7 kere yazmak yerine for döngüsü yazdık
+for (var i = 0; i<numberOfDrumButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+
+        var buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+// Detecting Keyboard Press
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key){
+    switch (key){ // {} parantez yerine : break; kullanıyoruz ikisi aynı şey.
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+        break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+        break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+        break;
+
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+        break;
+
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+        break;
+
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+        break;
+
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+        break;
+
+        default: console.log();
+    }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(
+        function(){
+            activeButton.classList.remove("pressed");
+        },100
+    );
+
+}
+
+
+// var audio =new Audio("sounds/tom-1.mp3"); // Audio sınıfından audio objesi/nesnesi oluşturulur
+// audio.play();
+//alert("slm");
+
+// document.querySelectorAll("button")[0].addEventListener("click",handleClick); // ilk button u seçiyor
+// fonksiyon, başka bir zaman tetiklenmek üzere input olarak verilirken parantez kullanılmaz
+// parantezi koyarsak fonksiyon hemen tetiklenir
+
+// Anonim fonksiyon versiyonu
+// document.querySelector("button").addEventListener("click",function(){});
+
+//function handleClick(){
+//    alert("I got clicked!");
+//}
